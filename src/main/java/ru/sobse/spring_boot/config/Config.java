45324788139abd3fbcc8471931cec6733aa30f7e
Sobolev Sergey <shutle@gmail.com>
2023-08-21 08:49:48 +0300

    Fix default property dev

--- src/main/java/ru/sobse/spring_boot/config/Config.java
+++ src/main/java/ru/sobse/spring_boot/config/Config.java
@@ -1,18 +1,17 @@
 package ru.sobse.spring_boot.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import ru.sobse.spring_boot.systemprofile.DevProfile;
 import ru.sobse.spring_boot.systemprofile.ProductionProfile;
 import ru.sobse.spring_boot.systemprofile.SystemProfile;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
 
     @Bean
-    @ConditionalOnProperty(prefix = "profile", name = "dev", havingValue = "true")
+    @ConditionalOnProperty(prefix = "profile", name = "dev", havingValue = "true", matchIfMissing = true)
     public SystemProfile devProfile() {
         return new DevProfile();
     }
